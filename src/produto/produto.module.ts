4e61a7738dcef86d1b87ae9323a42a173f012018
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-03-02 23:54:08 -0400

    update

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -3,7 +3,6 @@ import { ProdutoService } from './produto.service';
 import { ProdutoController } from './produto.controller';
 import { DbModule } from 'src/db/db.module';
 @Module({
-  imports: [DbModule],
   controllers: [ProdutoController],
   providers: [ProdutoService],
 })
